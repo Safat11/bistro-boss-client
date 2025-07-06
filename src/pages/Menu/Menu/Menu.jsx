52535184c65903af4fbf2d2cac6231938e664f62
Safat11 <safat3622@gmail.com>
2025-07-07 00:04:20 +0600

    Create Custom Hook UseMenu To Load Menu Data

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 const Menu = () => {
     return (
         <div>
@@ -9,11 +9,6 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title="our menu"></Cover>
-            <PopularMenu></PopularMenu>
         </div>
     );
 };
