ca0ead4dfe9d993acaad210abe33ce9dca893bf5
Safat11 <safat3622@gmail.com>
2025-07-08 22:23:25 +0600

    Load Menu Data From Server

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,16 +1,16 @@
 
 
-const FoodCard = ({item}) => {
-     const {name, image, price, recipe} = item;
+const FoodCard = ({ item }) => {
+    const { name, image, price, recipe } = item;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
             <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-            <div className="card-body text-center">
+            <div className="card-body flex flex-col items-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add To Cart</button>
+                    <button className="btn btn-outline hover:bg-gray-400 bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add To Cart</button>
                 </div>
             </div>
         </div>
