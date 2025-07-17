869fd96dfaa6ac2f9b37d9857de1b0fe12600758
Safat11 <safat3622@gmail.com>
2025-07-17 12:06:30 +0600

    Create a Dashboard Route And Dashboard Navigation

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,12 @@
-
+import { Link, Outlet } from "react-router-dom";
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
 
 const Dashboard = () => {
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
+                <Outlet></Outlet>
                 {/* Page content here */}
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
                     Open drawer
@@ -14,8 +16,15 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                     {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+
+                    <li><Link><FaHome></FaHome>  User Home</Link></li>
+                    <li><Link><FaCalendarAlt></FaCalendarAlt> Reservations</Link></li>
+                    <li><Link><FaWallet></FaWallet> Payment History</Link></li>
+                    <li><Link><FaShoppingCart></FaShoppingCart> My Cart</Link></li>
+                    <div className="divider"></div>
+                    <li><Link to="/"><FaHome></FaHome> Home</Link></li>
+                    <li><Link to="/menu">Our Menu</Link></li>
+                    <li><Link to="/order/salad">Order Food</Link></li>
                 </ul>
             </div>
         </div>
