a9cb5a04178d0572cbbe6e5b0c99015ed933e1f3
Safat11 <safat3622@gmail.com>
2025-07-23 00:35:05 +0600

    Upload Image To Image Hosting Server Imgbb And Get Image Url

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -1,10 +1,28 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
 
+const img_hosting_token = import.meta.env.VITE_Image_Upload_Token;
+
 const AddItem = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
+    const onSubmit = data => {
+
+        const formData = new FormData();
+        formData.append('image', data.image[0])
+
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgResponse => {
+                console.log(imgResponse);
+            })
+    };
     console.log(errors);
+    console.log(img_hosting_token)
     return (
         <div className="w-full px-10">
             <SectionTitle subHeading="What's new" heading="Add an Item"></SectionTitle>
@@ -17,8 +35,8 @@ const AddItem = () => {
                 <div className="flex my-4">
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Category*</legend>
-                        <select {...register("category", { required: true })} defaultValue="Pick a browser" className="select">
-                            <option disabled={true}>Pick one</option>
+                        <select {...register("category", { required: true })} defaultValue="Pick One" className="select">
+                            <option disabled={true}>Pick One</option>
                             <option>Salad</option>
                             <option>Pizza</option>
                             <option>Soup</option>
