e58e596cfc4db347304164ee15b9e48aa5ddcc4e
Safat11 <safat3622@gmail.com>
2025-07-23 00:04:11 +0600

    Where To Store Images With Pros And Cons

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -9,33 +9,35 @@ const AddItem = () => {
         <div className="w-full px-10">
             <SectionTitle subHeading="What's new" heading="Add an Item"></SectionTitle>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <fieldset className="fieldset">
+                <fieldset className="fieldset mb-4">
                     <legend className="fieldset-legend font-semibold">Recipe name*</legend>
                     <input type="text" className="input" placeholder="Recipe Name"
-                    {...register("name", { required: true, maxLength: 120 })} />
+                        {...register("name", { required: true, maxLength: 120 })} />
                 </fieldset>
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend">Category*</legend>
-                    <select {...register("category", { required: true })} defaultValue="Pick a browser" className="select">
-                        <option disabled={true}>Pick one</option>
-                        <option>Salad</option>
-                        <option>Pizza</option>
-                        <option>Soup</option>
-                        <option>Dessert</option>
-                        <option>Drinks</option>
-                    </select>
-                </fieldset>
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend font-semibold">Price*</legend>
-                    <input type="number" {...register("price", { required: true })} className="input" placeholder="Type here" />
-                </fieldset>
+                <div className="flex my-4">
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Category*</legend>
+                        <select {...register("category", { required: true })} defaultValue="Pick a browser" className="select">
+                            <option disabled={true}>Pick one</option>
+                            <option>Salad</option>
+                            <option>Pizza</option>
+                            <option>Soup</option>
+                            <option>Dessert</option>
+                            <option>Drinks</option>
+                        </select>
+                    </fieldset>
+                    <fieldset className="fieldset ml-4">
+                        <legend className="fieldset-legend font-semibold">Price*</legend>
+                        <input type="number" {...register("price", { required: true })} className="input" placeholder="Type here" />
+                    </fieldset>
+                </div>
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend">Recipe Details</legend>
-                    <textarea {...register("details", { required: true })} className="textarea h-24" placeholder="Bio"></textarea>
+                    <textarea {...register("recipe", { required: true })} className="textarea h-24" placeholder="Bio"></textarea>
                 </fieldset>
-                <fieldset className="fieldset">
+                <fieldset className="fieldset my-4">
                     <legend className="fieldset-legend">Item Image*</legend>
-                    <input type="file" className="file-input" />
+                    <input type="file" {...register("image", { required: true })} className="file-input" />
                     {/* <label className="label">Max size 2MB</label> */}
                 </fieldset>
                 <input className="btn btn-sm mt-4" type="submit" value="Add Item" />
