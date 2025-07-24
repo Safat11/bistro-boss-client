7a55bc99f168b859d3ddfa4311683cd821138350
Safat11 <safat3622@gmail.com>
2025-07-25 01:05:33 +0600

    Use Confirm Card Payment To Process Payment

--- src/pages/Dashboard/Payment/__payment_integration_steps__.js
+++ src/pages/Dashboard/Payment/__payment_integration_steps__.js
@@ -1,4 +1,4 @@
-/**
+/* *
  * 1. install stripe and react stripe js
  * 2. create a checkout form with card element (card element contains: card number, expiration date, cvs, zip code)
  * 3. create account on Stripe and get the publishable key pk
@@ -8,5 +8,6 @@
  * 7. on the server side: install stripe: npm install --save stripe
  * 8. create a payment intent api with payment method types: ['card']
  * make sure you provide amount in cents (multiply price with 100)
- * 
-*/
+ * 9. call payment intent api to get client secret and store it in a state
+ * 10. use confirmCardPayment api with client secret card info 
+ */
