0da706dd4979c4fb43cc0a8c516439e8de562aac
Safat11 <safat3622@gmail.com>
2025-07-25 01:47:35 +0600

    Display Payment Error And Success Message

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -14,13 +14,17 @@ const CheckoutForm = ({ price }) => {
 
     const [clientSecret, setClientSecret] = useState();
 
+    const [processing, setProcessing] = useState(false);
+
+    const [transactionId, setTransactionId] = useState('');
+
     useEffect(() => {
         axiosSecure.post('/create-payment-intent', { price })
             .then(res => {
                 console.log(res.data.clientSecret)
                 setClientSecret(res.data.clientSecret);
             })
-    }, [price, axiosSecure])
+    }, [])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -45,9 +49,10 @@ const CheckoutForm = ({ price }) => {
         }
         else {
             setCardError('');
-            console.log('payment method', paymentMethod)
+            // console.log('payment method', paymentMethod)
         }
 
+        setProcessing(true)
         const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
@@ -62,10 +67,16 @@ const CheckoutForm = ({ price }) => {
             console.log(confirmError);
         }
 
-        console.log(paymentIntent);
+        console.log("payment intent", paymentIntent);
 
+        setProcessing(false)
+
+        if (paymentIntent.status === 'succeeded') {
+        setTransactionId(paymentIntent.id);
+            // TODO next steps
+
+        }
     }
-
     return (
         <>
             <form className="w-2/3 m-8" onSubmit={handleSubmit}>
@@ -85,13 +96,14 @@ const CheckoutForm = ({ price }) => {
                         },
                     }}
                 />
-                <button className="btn btn-primary btn-sm mt-4" type="submit" disabled={!stripe || !clientSecret}>
+                <button className="btn btn-primary btn-sm mt-4" type="submit" disabled={!stripe || !clientSecret || processing}>
                     Pay
                 </button>
             </form>
             {
                 cardError && <p className="text-red-600 ml-8">{cardError}</p>
             }
+            {transactionId && <p className="text-green-500">Transaction complete with transactionId: {transactionId}</p>}
         </>
     );
 };
